7f5e86d6838b3893bcd661229f6612f7ff70d6ff
Nasrullah <nasrullah01n@gmail.com>
2022-12-30 18:36:47 +0800

    Initial set up of flutter project

---
+++ android/app/src/main/kotlin/com/example/clicktoeat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clicktoeat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ClickToEat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Hello world")),
+      body: Container(
+        alignment: Alignment.center,
+        child: const Text("Hello World"),
+      ),
+    );
+  }
+}
