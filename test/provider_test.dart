3357ba3e7a829b48a0325f89843208f012363f2a
Nasrullah <nasrullah01n@gmail.com>
2023-02-05 17:12:52 +0800

    Added some ui testing for provider

---
+++ test/provider_test.dart
@@ -0,0 +1,88 @@
+import 'package:clicktoeat/providers/auth_provider.dart';
+import 'package:clicktoeat/providers/user_provider.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'fakes/fakes.dart';
+
+void main() {
+  group('User Provider', () {
+    late UserProvider provider;
+    late FakeUserRepo fakeUserRepo;
+    setUpAll(() {
+      fakeUserRepo = FakeUserRepo();
+      provider = UserProvider(fakeUserRepo);
+    });
+
+    test('should return a list of users', () async {
+      await provider.getUsers();
+      expect(provider.users, isNotEmpty);
+      expect(provider.users.length, fakeUserRepo.users.length);
+    });
+
+    test("should return one user if id is valid", () async {
+      var user = await provider.getUserById("1");
+      expect(user, isNotNull);
+      expect(user.id, "1");
+    });
+  });
+
+  group('Auth Provider', () {
+    late AuthProvider authProvider;
+    late UserProvider userProvider;
+    late FakeUserRepo fakeUserRepo;
+    setUpAll(() {
+      fakeUserRepo = FakeUserRepo();
+      userProvider = UserProvider(fakeUserRepo);
+      authProvider = AuthProvider(fakeUserRepo, userProvider);
+    });
+
+    test('should return a token', () async {
+      await authProvider.getToken();
+      expect(authProvider.token, isNotNull);
+      expect(authProvider.token == fakeUserRepo.token, isTrue);
+    });
+
+    test('should return a user', () async {
+      await authProvider.getCurrentUser();
+      expect(authProvider.user, isNotNull);
+      expect(authProvider.user!.id, fakeUserRepo.users.last.id);
+    });
+
+    test(
+      'When login, with valid credentials, should update token',
+      () async {
+        var oldToken = authProvider.token;
+        var lastUser = fakeUserRepo.users.last;
+        await authProvider.login(lastUser.email, "fasdfasdfasdf");
+        expect(authProvider.token, isNotNull);
+        expect(authProvider.token, isNot(oldToken));
+      },
+    );
+
+    test(
+      'When create account, should update token and users',
+      () async {
+        var oldToken = authProvider.token;
+        await authProvider.register("test", "test@test.com", "fasdfasdfasdf");
+        expect(authProvider.token, isNotNull);
+        expect(authProvider.token, isNot(oldToken));
+        expect(authProvider.user, isNotNull);
+        expect(authProvider.user?.email == "test@test.com", isTrue);
+        expect(userProvider.users.length, fakeUserRepo.users.length);
+        expect(userProvider.users.last.email == "test@test.com", isTrue);
+      },
+    );
+
+    test(
+      'When logout, should update token and users',
+      () async {
+        var oldToken = authProvider.token;
+        await authProvider.logOut();
+        expect(authProvider.token, isNull);
+        expect(authProvider.token, isNot(oldToken));
+        expect(authProvider.user, isNull);
+        expect(userProvider.users.length, fakeUserRepo.users.length);
+      },
+    );
+  });
+}
